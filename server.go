2f79a44c1400f4b7431ebabdc3e1c9206a5136e3
Moanrisy <moanrisy@gmail.com>
2022-12-13 13:57:24 +0700

    Setup postgresql db in supabase with .env file

--- server.go
+++ server.go
@@ -1,13 +1,42 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/pug"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+func connectDB() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	password := os.Getenv("PASSWORD")
+
+	// Open a connection to the database
+	dbinfo := fmt.Sprintf("user=postgres password=%s host=db.fiakzcmolcxandnyppnp.supabase.co port=5432 dbname=postgres", password)
+	db, err := sql.Open("postgres", dbinfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Try to ping the database
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Successfully connected to the database!")
+}
+
 func main() {
+
+	connectDB()
 
 	// Create a new engine
 	engine := pug.New("./views", ".pug")
@@ -17,7 +46,7 @@ func main() {
 		Views: engine,
 	})
 
-	app.Static("/", "./public") 
+	app.Static("/", "./public")
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		// Render index
