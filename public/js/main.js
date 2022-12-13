fe29557838de4632d7b3dd94ae721688bd957301
Moanrisy <moanrisy@gmail.com>
2022-12-13 19:42:18 +0700

    Fix annoying BUG with hard code date

--- public/js/main.js
+++ public/js/main.js
@@ -20,20 +20,20 @@ const squares = document.querySelector(".squares");
 
 // TODO use this snippet to compare 2 different date options
 // const dateString = new Date();
-const dateString = currentDate.toLocaleDateString('en-US', {
-  day: 'numeric',
-  month: 'numeric',
-  year: 'numeric'
+const dateString = currentDate.toLocaleDateString("en-US", {
+  day: "numeric",
+  month: "numeric",
+  year: "numeric",
 });
-console.log(dateString)
+// console.log(dateString)
 
-const date2String = currentDate.toLocaleDateString("en-US", options)
+const date2String = currentDate.toLocaleDateString("en-US", options);
 
-const test1 = new Date(dateString)
-const test2 = new Date(date2String)
-const date1ISOString = test1.toISOString()
-const date2ISOString = test2.toISOString() 
-console.log(date1ISOString == date2ISOString)
+const test1 = new Date(dateString);
+const test2 = new Date(date2String);
+const date1ISOString = test1.toISOString();
+const date2ISOString = test2.toISOString();
+// console.log(date1ISOString == date2ISOString)
 
 for (var i = 1; i < 367; i++) {
   const formattedDate = currentDate.toLocaleDateString("en-US", options);
@@ -56,12 +56,40 @@ for (var i = 1; i < 367; i++) {
     }
   }
 
+  const matchedDate = currentDate.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "numeric",
+    year: "numeric",
+  });
+
+  for (var j = 0; j < pomodoros.length; j++) {
+    var exactDate = JSON.stringify(pomodoros[j].date);
+    exactDate = exactDate.replace(/"/g, "");
+
+    // console.log(matchedDate)
+
+    if (exactDate == matchedDate) {
+      // console.log(exactDate);
+    }
+  }
+
   // Move to the next day
   currentDate.setDate(currentDate.getDate() + 1);
   // If we are at the last day of the current month, move to the first day of the next month
   if (currentDate.getDate() === 1) {
     currentDate.setDate(1);
+
+    console.log(currentDate.getMonth());
+    if (currentDate.getMonth() - 2 == -1) {
+      console.log("finally dsemeber again");
+      currentDate.setUTCFullYear(2023, 01)
+      // currentDate.setMonth(currentDate.getMonth() - 1)
+      // currentDate.setMonth(12)
+    }
     currentDate.setMonth(currentDate.getMonth() - 2);
+    console.log(
+      currentDate.getMonth() - 2 + " " + currentDate.toLocaleDateString("en-US")
+    );
   }
 
   // If we have reached the 11th previous month, break out of the loop
@@ -82,6 +110,7 @@ for (var i = 1; i < 367; i++) {
 
   // TODO update with real database
   const level = Math.floor(Math.random() * 3);
+
   squares.insertAdjacentHTML(
     "beforeend",
     `<li data-date="${i}" data-level="${level}">          <span class="tooltiptext">${formattedDate}</span> </li>`
